df52a356ff41cd75f2e64f27bbcbd19d45024818
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2020-06-18 13:26:49 +0900

    コンポーネントの作成

--- app/google-map-demo/src/App.vue
+++ app/google-map-demo/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GoogleMapComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GoogleMapComponent from "./components/GoogleMapComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    GoogleMapComponent
   }
-}
+};
 </script>
 
 <style>
---
+++ app/google-map-demo/src/components/GoogleMapComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main"></div>
+</template>
+
+<script>
+export default {
+  name: "GoogleMapComponent"
+};
+</script>
+
+<style scoped>
+</style>
